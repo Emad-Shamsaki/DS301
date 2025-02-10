6760ac1b500b06b39ae808165bee3d453516b78d
emad <e.shamsaki@inelelettronica.it>
2025-02-10 10:49:20 +0100

    fix read all

--- hal_can_inel/hal_can_inel.c
+++ hal_can_inel/hal_can_inel.c
@@ -390,7 +390,45 @@ int hci_export(HCI_t* _pHCI)
 // 		}	
 // 	}
 // }
-void hci_read_all(void *_void_hci, long _period) {
+// void hci_read_all(void *_void_hci, long _period) {
+//     HCI_t* _pHCI = (HCI_t*)_void_hci;
+//     struct can_frame _frame;
+//     int _iRead;
+//     __u32 _can_id;
+//     __u32 _Counter;
+
+//     _pHCI->HalPars.dwReadExecCount++;
+
+//     while ((_iRead = recv(_pHCI->iCanSocket, &_frame, sizeof(_frame), MSG_DONTWAIT)) > 0) {
+//         if ((_frame.can_id & CAN_EFF_FLAG) == 0) {
+//             _can_id = _frame.can_id & CAN_SFF_MASK;
+
+//             if ((_can_id == 0x190) && (_frame.len == 8)) {
+//                 printf("Received CAN message 0x190: ");
+//                 for (int i = 0; i < 8; i++)
+//                     printf("%02X ", _frame.data[i]);
+//                 printf("\n");
+
+//                 _Counter = _frame.data[1] | (_frame.data[2] << 8);
+//                 printf("Counter 0: %u\n", _Counter);
+//                 *_pHCI->HalPins.pdwCounters[0] = _Counter;
+
+//                 _Counter = _frame.data[4] | (_frame.data[5] << 8);
+//                 printf("Counter 1: %u\n", _Counter);
+//                 *_pHCI->HalPins.pdwCounters[1] = _Counter;
+
+//                 printf("Digital Inputs Byte 6: %02X, Byte 7: %02X\n", _frame.data[6], _frame.data[7]);
+
+//                 for (int i = 0; i < 8; i++) {
+//                     *_pHCI->HalPins.pbitDigInputs[i] = (_frame.data[6] & (1 << i)) ? 1 : 0;
+//                     *_pHCI->HalPins.pbitDigInputs[i + 8] = (_frame.data[7] & (1 << i)) ? 1 : 0;
+//                 }
+//             }
+//         }
+//     }
+// }
+void hci_read_all(void *_void_hci, long _period) 
+{
     HCI_t* _pHCI = (HCI_t*)_void_hci;
     struct can_frame _frame;
     int _iRead;
@@ -399,34 +437,67 @@ void hci_read_all(void *_void_hci, long _period) {
 
     _pHCI->HalPars.dwReadExecCount++;
 
-    while ((_iRead = recv(_pHCI->iCanSocket, &_frame, sizeof(_frame), MSG_DONTWAIT)) > 0) {
-        if ((_frame.can_id & CAN_EFF_FLAG) == 0) {
+    while ((_iRead = recv(_pHCI->iCanSocket, &_frame, sizeof(_frame), MSG_DONTWAIT)) > 0) 
+    {   
+        if ((_frame.can_id & CAN_EFF_FLAG) == 0) 
+        {   
             _can_id = _frame.can_id & CAN_SFF_MASK;
 
-            if ((_can_id == 0x190) && (_frame.len == 8)) {
-                printf("Received CAN message 0x190: ");
-                for (int i = 0; i < 8; i++)
-                    printf("%02X ", _frame.data[i]);
-                printf("\n");
+            // Debugging: Print received CAN frame
+            printf("Received CAN ID: 0x%X Data:", _can_id);
+            for (int i = 0; i < _frame.len; i++)
+                printf(" %02X", _frame.data[i]);
+            printf("\n");
 
-                _Counter = _frame.data[1] | (_frame.data[2] << 8);
-                printf("Counter 0: %u\n", _Counter);
+            // Process Digital Inputs and Counters (ID: 0x190)
+            if ((_can_id == (0x180 + _pHCI->HalPars.dwWAGO_ID)) && (_frame.len == 8)) 
+            {   
+                // Read Digital Inputs (1 Byte Each)
+                *_pHCI->HalPins.pbitDigInputs[0] = (_frame.data[0] & 0x01) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[1] = (_frame.data[0] & 0x02) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[2] = (_frame.data[0] & 0x04) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[3] = (_frame.data[0] & 0x08) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[4] = (_frame.data[0] & 0x10) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[5] = (_frame.data[0] & 0x20) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[6] = (_frame.data[0] & 0x40) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[7] = (_frame.data[0] & 0x80) ? 1 : 0;
+
+                *_pHCI->HalPins.pbitDigInputs[8] = (_frame.data[1] & 0x01) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[9] = (_frame.data[1] & 0x02) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[10] = (_frame.data[1] & 0x04) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[11] = (_frame.data[1] & 0x08) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[12] = (_frame.data[1] & 0x10) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[13] = (_frame.data[1] & 0x20) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[14] = (_frame.data[1] & 0x40) ? 1 : 0;
+                *_pHCI->HalPins.pbitDigInputs[15] = (_frame.data[1] & 0x80) ? 1 : 0;
+
+                // Read Counters (3 Bytes Each)
+                _Counter = _frame.data[2] | (_frame.data[3] << 8) | (_frame.data[4] << 16);
                 *_pHCI->HalPins.pdwCounters[0] = _Counter;
-
-                _Counter = _frame.data[4] | (_frame.data[5] << 8);
                 printf("Counter 1: %u\n", _Counter);
+
+                _Counter = _frame.data[5] | (_frame.data[6] << 8) | (_frame.data[7] << 16);
                 *_pHCI->HalPins.pdwCounters[1] = _Counter;
-
-                printf("Digital Inputs Byte 6: %02X, Byte 7: %02X\n", _frame.data[6], _frame.data[7]);
-
-                for (int i = 0; i < 8; i++) {
-                    *_pHCI->HalPins.pbitDigInputs[i] = (_frame.data[6] & (1 << i)) ? 1 : 0;
-                    *_pHCI->HalPins.pbitDigInputs[i + 8] = (_frame.data[7] & (1 << i)) ? 1 : 0;
-                }
+                printf("Counter 2: %u\n", _Counter);
             }
-        }
+
+            // Process Additional Counters (ID: 0x290)
+            else if ((_can_id == (0x280 + _pHCI->HalPars.dwWAGO_ID)) && (_frame.len == 6)) 
+            {   
+                // Read Counter 3
+                _Counter = _frame.data[0] | (_frame.data[1] << 8) | (_frame.data[2] << 16);
+                *_pHCI->HalPins.pdwCounters[2] = _Counter;
+                printf("Counter 3: %u\n", _Counter);
+
+                // Read Counter 4
+                _Counter = _frame.data[3] | (_frame.data[4] << 8) | (_frame.data[5] << 16);
+                *_pHCI->HalPins.pdwCounters[3] = _Counter;
+                printf("Counter 4: %u\n", _Counter);
+            }
+        }   
     }
 }
+
 
 
 void hci_write_all(void *_void_hci, long _period) 
